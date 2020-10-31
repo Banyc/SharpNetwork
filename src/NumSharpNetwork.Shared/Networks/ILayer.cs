1a87e34d6b6ff6f7cb362ddcf2504ed47b72e7a0
Banyc <36535895+Banyc@users.noreply.github.com>
2020-11-01 01:04:06 +0800

    [term] fix term error

--- src/NumSharpNetwork.Shared/Networks/ILayer.cs
+++ src/NumSharpNetwork.Shared/Networks/ILayer.cs
@@ -10,7 +10,7 @@ namespace NumSharpNetwork.Shared.Networks
         // feedforward
         NDarray FeedForward(NDarray input);
         // backpropagation
-        NDarray BackPropagate(NDarray resultLossGradient);
+        NDarray BackPropagate(NDarray lossResultGradient);
         // save necessary state on disk
         void Save(string folderPath);
         // load state on disk if file exists
