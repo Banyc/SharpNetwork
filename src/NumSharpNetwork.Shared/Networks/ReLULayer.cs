e40bac8240c222c29560743e615958ea84730e0a
Banyc <36535895+Banyc@users.noreply.github.com>
2020-11-01 01:19:36 +0800

    [ReLU] Add ReLU layer

---
+++ src/NumSharpNetwork.Shared/Networks/ReLULayer.cs
@@ -0,0 +1,33 @@
+using Numpy;
+
+namespace NumSharpNetwork.Shared.Networks
+{
+    public class ReLULayer : ILayer
+    {
+        public string Name { get; set; }
+        public bool IsTrainMode { get; set; }
+        private NDarray PreviousInput { get; set; }
+
+        public NDarray BackPropagate(NDarray lossResultGradient)
+        {
+            NDarray resultInputGradient = np.where(this.PreviousInput <= 0, np.asarray(0), np.asarray(1));
+
+            NDarray lossInputGradient = lossResultGradient * resultInputGradient;
+            return lossInputGradient;
+        }
+
+        public NDarray FeedForward(NDarray input)
+        {
+            this.PreviousInput = input;
+            return np.maximum(np.asarray(0), input);
+        }
+
+        public void Load(string folderPath)
+        {
+        }
+
+        public void Save(string folderPath)
+        {
+        }
+    }
+}
