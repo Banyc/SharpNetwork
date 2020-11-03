21b02003213b0be4c5e6fcc0209d880dbb726170
Banyc <36535895+Banyc@users.noreply.github.com>
2020-11-03 20:02:12 +0800

    [Layer] Add flatten layer

---
+++ src/NumSharpNetwork.Shared/Networks/FlattenLayer.cs
@@ -0,0 +1,41 @@
+using Numpy;
+
+namespace NumSharpNetwork.Shared.Networks
+{
+    public class FlattenLayer : ILayer
+    {
+        public string Name { get; set; }
+        public bool IsTrainMode { get; set; }
+        public NDarray PreviousInput { get; set; }
+
+        public NDarray BackPropagate(NDarray lossResultGradient)
+        {
+            return lossResultGradient.reshape(
+                this.PreviousInput.shape[0],
+                this.PreviousInput.shape[1],
+                this.PreviousInput.shape[2],
+                this.PreviousInput.shape[3]
+            );
+        }
+
+        // input.shape = [batchSize, channels, height, width]
+        // return shape = [batchSize, channels * height * width]
+        public NDarray FeedForward(NDarray input)
+        {
+            int batchSize = input.shape.Dimensions[0];
+            NDarray result = input.reshape(batchSize, -1);
+
+            this.PreviousInput = result;
+
+            return result;
+        }
+
+        public void Load(string folderPath)
+        {
+        }
+
+        public void Save(string folderPath)
+        {
+        }
+    }
+}
