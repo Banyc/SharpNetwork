477837d29b396a3e69d7f2ccffc3cc4a55cecdee
Banyc <36535895+Banyc@users.noreply.github.com>
2020-11-07 17:29:46 +0800

    [wrapped layers] Add linear layers for image

---
+++ src/NumSharpNetwork.Shared/Networks/Wrappers/ImageLinearLayers.cs
@@ -0,0 +1,30 @@
+using NumSharpNetwork.Shared.Optimizers;
+
+namespace NumSharpNetwork.Shared.Networks.Wrappers
+{
+    public class ImageLinearLayers : NetworkWrapper
+    {
+        public override string Name { get; set; } = "ImageLinearLayers";
+
+        public ImageLinearLayers(int height, int width, int channels, int numClasses, IOptimizer optimizer)
+        {
+            FlattenLayer flatten = new FlattenLayer();
+
+            int linearInputSize = (height) * (width) * channels;
+
+            LinearLayer linear1 = new LinearLayer(optimizer, linearInputSize, 100, name: $"{this.Name}.LinearLayer1");
+
+            ReLULayer reLU1 = new ReLULayer();
+
+            LinearLayer linear2 = new LinearLayer(optimizer, 100, numClasses, name: $"{this.Name}.LinearLayer2");
+
+            SoftmaxLayer softmax = new SoftmaxLayer();
+
+            this.Layers.Add(flatten);
+            this.Layers.Add(linear1);
+            this.Layers.Add(reLU1);
+            this.Layers.Add(linear2);
+            this.Layers.Add(softmax);
+        }
+    }
+}
